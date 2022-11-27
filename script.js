8ebfb61e975cd2fe45a9e7be8e5d0a48d1c3dfe8
agdemirayhangit <ayhanagdemir@hotmail.com.tr>
2022-11-27 23:00:53 +0300

    Media Queries were implemented

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
 "use strict";
 
-const body = document.querySelector("body");
-const board = document.querySelector(".board");
 const squares = document.querySelectorAll(".symbol");
 const container = document.querySelector(".container");
 const winScreen = document.querySelector(".win-screen");
 const reset = document.querySelector(".reset");
 const turnScreen = document.querySelector(".turn");
-
-// board.addEventListener("click", function () {
-//   // console.log(board.getBoundingClientRect().left);
-//   // console.log(board.getBoundingClientRect().top);
-//   // console.log(board.getBoundingClientRect().width);
-// });
-
-// board.addEventListener("click", function (e) {
-//   const onBoardCoordsX = e.clientX - board.getBoundingClientRect().left;
-//   const onBoardCoordsY = e.clientY - board.getBoundingClientRect().top;
-//   console.log(onBoardCoordsX, onBoardCoordsY);
-//   console.log(`X : ${Math.floor(onBoardCoordsX / 200) + 1}`);
-//   console.log(`Y : ${Math.floor(onBoardCoordsY / 200) + 1}`);
-//   // placeSymbol();
-// });
 
 let xturn = true;
 let xArr = [];
@@ -38,21 +21,8 @@ const winningConditions = [
   [2, 5, 8],
 ];
 
-console.log(winningConditions);
-
-function placeSymbol(e) {
-  if (e.innerHTML === "") {
-    if (xturn) {
-      e.innerHTML = `<img src="/x.png" alt="">`;
-    } else {
-      e.innerHTML = `<img src="/o4.png" alt="">`;
-    }
-  }
-}
-
 squares.forEach((e, idx) => {
   e.addEventListener("click", function () {
-    // placeSymbol(e);
     if (e.innerHTML === "" && win === false) {
       placeSymbol(e);
       if (xturn) {
@@ -66,33 +36,9 @@ squares.forEach((e, idx) => {
         xturn = true;
         turnScreen.innerHTML = `X's TURN`;
       }
-      console.log(xArr);
-      console.log(oArr);
     }
   });
 });
-
-function checkWin() {
-  winningConditions.forEach((e) => {
-    const XInt = e.filter((a) => xArr.includes(a));
-    const OInt = e.filter((a) => oArr.includes(a));
-    if (XInt.length === 3) {
-      winScreen.classList.add("show");
-      winScreen.innerHTML = `<span>X WON</span>`;
-      win = true;
-      container.classList.add("win");
-    } else if (OInt.length === 3) {
-      winScreen.classList.add("show");
-      winScreen.innerHTML = `<span>O WON</span>`;
-      win = true;
-      container.classList.add("win");
-    } else if (xArr.length === 5 && win === false) {
-      winScreen.classList.add("show");
-      winScreen.innerHTML = `<span>DRAW</span>`;
-      container.classList.add("win");
-    }
-  });
-}
 
 reset.addEventListener("click", function () {
   xArr = [];
@@ -107,3 +53,35 @@ reset.addEventListener("click", function () {
     turnScreen.innerHTML = `X's TURN`;
   });
 });
+
+function placeSymbol(e) {
+  if (e.innerHTML === "") {
+    if (xturn) {
+      e.innerHTML = `<img src="/x1.png" alt="">`;
+    } else {
+      e.innerHTML = `<img src="/o4.png" alt="">`;
+    }
+  }
+}
+
+function checkWin() {
+  winningConditions.forEach((e) => {
+    const XInt = e.filter((a) => xArr.includes(a));
+    const OInt = e.filter((a) => oArr.includes(a));
+    if (XInt.length === 3) {
+      showResult("X WON");
+      win = true;
+    } else if (OInt.length === 3) {
+      showResult("O WON");
+      win = true;
+    } else if (xArr.length === 5 && win === false) {
+      showResult("DRAW");
+    }
+  });
+}
+
+function showResult(x) {
+  winScreen.classList.add("show");
+  winScreen.innerHTML = `<span>${x}</span>`;
+  container.classList.add("win");
+}
